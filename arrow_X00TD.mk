af07588e5f8978a65214e9a81b2894d15fd9fda7
Aditya <adiatul16@gmail.com>
2020-12-10 15:45:46 +0530

    X00TD: Adapt for Arrow OS

--- arrow_X00TD.mk
+++ arrow_X00TD.mk
@@ -9,14 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow OS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+TARGET_GAPPS_ARCH := arm64
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := arrow_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
